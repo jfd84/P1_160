b1adc54611f8497dcaa6ec8e1745acfde00bd56e
jfd84 <jfd84@georgetown.edu>
2022-09-05 11:05:48 -0400

    Initial Commit

---
+++ P1_160/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  P1_160
+//
+//  Created by Joe D'Agostino on 9/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
